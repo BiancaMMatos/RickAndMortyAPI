e25888613c7d5e8830f83469117e43bb6e20ac55
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2022-12-19 18:43:01 -0300

    Add files via upload

---
+++ RickAndMortyAPI/RickAndMortyAPI/RickAndMortyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAPIApp.swift
+//  RickAndMortyAPI
+//
+//  Created by Bianca Maciel Matos on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
